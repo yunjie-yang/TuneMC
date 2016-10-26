65d66feb606168ec0c022b9d0e7cf642c4c73d63
Yunjie Yang <yunjiey@mit.edu>
2016-10-25 23:56:54 -0400

    modified

--- master.py
+++ master.py
@@ -1,2 +1,15 @@
-import numpy as np
+import sys
+import time
 import os
+
+import numpy as np
+
+try: import simplejson as json
+except ImportError: import json
+
+def get_options():
+
+    config_file = os.path.join()
+
+    try:
+	with open()
